3aaad8b9ecb4835a739499bce2bcbc2832178d7c
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2025-05-15 01:14:29 +0700

    Fix lib

--- src/lib/server/serverSocket.ts
+++ src/lib/server/serverSocket.ts
@@ -1,4 +1,4 @@
-import { createServer } from "http";
+import { createServer } from "node:http";
 import { Server, Socket } from "socket.io";
 import { UserManager } from "$lib/services/userManager";
 import type { Active } from "$lib/types";
