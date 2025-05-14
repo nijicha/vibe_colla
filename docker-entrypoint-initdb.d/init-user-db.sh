a7d55665e8b4ef63f89e04aa7e69ec545635c98c
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2025-05-15 00:17:41 +0700

    Rename project

--- docker-entrypoint-initdb.d/init-user-db.sh
+++ docker-entrypoint-initdb.d/init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 /opt/homebrew/opt/libpq/bin/psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE USER freehoot WITH PASSWORD '$POSTGRES_PASSWORD';
-	CREATE DATABASE freehoot_development;
-	GRANT ALL PRIVILEGES ON DATABASE freehoot_development TO freehoot;
+  CREATE USER '$POSTGRES_USER' WITH PASSWORD '$POSTGRES_PASSWORD';
+	CREATE DATABASE '${POSTGRES_USER}_development';
+	GRANT ALL PRIVILEGES ON DATABASE '${POSTGRES_USER}_development' TO '$POSTGRES_USER';
 EOSQL
